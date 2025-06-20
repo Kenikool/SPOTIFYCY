1e411ec447f0cbd759a42563682c394f258454f1
Kenikool <114859959+Kenikool@users.noreply.github.com>
2025-06-20 23:53:05 +0100

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+export default function App() {
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton>
+          <Button className="cursor-pointer ">Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
+}

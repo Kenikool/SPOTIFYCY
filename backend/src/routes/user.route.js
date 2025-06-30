f49918a208f16d0b67937dddaf8797f1169a96c2
Kenikool <114859959+Kenikool@users.noreply.github.com>
2025-06-30 09:58:21 +0100

    stat, users route and controllers

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { getAllUsers, getMessages } from "../controllers/user.controller.js";
+import { User } from "../models/user.model.js";
 const router = Router();
 
 router.get("/", protectRoute, getAllUsers);
 router.get("/messages/:userId", protectRoute, getMessages);
-
+router.get("/users", async (req, res, next) => {
+  try {
+    const users = await User.find({});
+  } catch (error) {}
+});
 export default router;

ee1a609aee097fcef16fc909a2d796b3126e8b2d
Kenikool <114859959+Kenikool@users.noreply.github.com>
2025-06-30 03:50:21 +0100

    Album routes and controllers

--- backend/src/controllers/album.controller.js
+++ backend/src/controllers/album.controller.js
@@ -1,2 +1,18 @@
-export const getAllAlbums = async (req, res, next) => {};
-export const getAlbumById = async (req, res, next) => {};
+import Album from "../models/album.model";
+
+export const getAllAlbums = async (req, res, next) => {
+  try {
+    const albums = await Album.find();
+    res.status(200).json(albums);
+  } catch (error) {}
+};
+export const getAlbumById = async (req, res, next) => {
+  try {
+    const { albumId } = req.params;
+    const album = await Album.findById(albumId).populate("songs");
+    if (!album) {
+      return res.status(404).json({ message: "Album not found" });
+    }
+    res.status(200).json(album);
+  } catch (error) {}
+};

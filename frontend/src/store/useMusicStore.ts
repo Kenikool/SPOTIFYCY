0546b1827f5d81980f0da4e6fc055417962fddfd
Kenikool <114859959+Kenikool@users.noreply.github.com>
2025-07-02 11:18:13 +0100

    Creating Albums Page

--- frontend/src/store/useMusicStore.ts
+++ frontend/src/store/useMusicStore.ts
@@ -1,18 +1,21 @@
 import { axiosInstance } from "./../lib/axios";
 import { create } from "zustand";
-import { Album, Song } from "../types";
+import type { Album, Song } from "@/types";
 interface MusicStore {
   songs: Song[];
   albums: Album[];
   isLoading: boolean;
   error: string | null;
+  currentAlbum: Album | null;
   fetchAlbums: () => Promise<void>; //Promise<void>, and we are not returning anything
+  fetchAlbumById: (id: string) => Promise<Album>;
 }
 export const useMusicStore = create<MusicStore>((set) => ({
   albums: [],
   songs: [],
   isLoading: false,
   error: null,
+  currentAlbum: null,
 
   fetchAlbums: async () => {
     set({ isLoading: true, error: null });
@@ -21,6 +24,19 @@ export const useMusicStore = create<MusicStore>((set) => ({
       const response = await axiosInstance.get("/albums");
 
       set({ albums: response.data });
+    } catch (error: any) {
+      set({ error: error.response.data.message });
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+  fetchAlbumById: async (id: string) => {
+    set({ isLoading: true, error: null });
+
+    try {
+      const response = await axiosInstance.get(`/albums/${id}`);
+
+      set({ currentAlbum: response.data });
     } catch (error: any) {
       set({ error: error.response.data.message });
     } finally {

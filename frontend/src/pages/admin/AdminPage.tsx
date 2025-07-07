a2854f69acef842877634bb7b05b7f6390cf019d
Kenikool <114859959+Kenikool@users.noreply.github.com>
2025-07-07 23:38:34 +0100

    Admin_05 Song table, songtabscontent

--- frontend/src/pages/admin/AdminPage.tsx
+++ frontend/src/pages/admin/AdminPage.tsx
@@ -1,15 +1,22 @@
 import { useAuthStore } from "@/store/useAuthStore";
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./components/Header";
 import DashboardStats from "./components/DashboardStats";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Album, Music } from "lucide-react";
 import SongTabContent from "./components/SongTabContent";
 import AlbumTabContent from "./components/AlbumTabContent";
+import { useMusicStore } from "@/store/useMusicStore";
 
 const AdminPage = () => {
   const { isAdmin, isLoading } = useAuthStore();
 
+  const { fetchAlbums, fetchSongs, fetchStats } = useMusicStore();
+  useEffect(() => {
+    fetchAlbums();
+    fetchSongs();
+    fetchStats();
+  }, [fetchAlbums, fetchSongs, fetchStats]);
   if (!isAdmin && !isLoading) return <div>Unauthorized</div>;
   return (
     <div className="min-h-screen bg-gradient-to-b from-zinc-900 via-zinc-900 to-black text-zinc-100 p-8">
---
+++ frontend/src/pages/admin/components/AddSongDialog.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddSongDialog = () => {
+  return <div>AddSongDialog</div>;
+};
+
+export default AddSongDialog;

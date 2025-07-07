c9d016269b4f50105bf7040f6f4538afb85ac391
Kenikool <114859959+Kenikool@users.noreply.github.com>
2025-07-07 20:17:29 +0100

    Admin_02 useMusicStore

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import MainLayout from "./layout/MainLayout";
 import ChatPage from "./pages/chat/ChatPage";
 import AlbumPage from "./pages/album/AlbumPage";
 import AdminPage from "./pages/admin/AdminPage";
+import { Toaster } from "react-hot-toast";
 export default function App() {
   return (
     <>
@@ -27,6 +28,7 @@ export default function App() {
           <Route path="/albums/:albumId" element={<AlbumPage />} />
         </Route>
       </Routes>
+      <Toaster />
     </>
   );
 }

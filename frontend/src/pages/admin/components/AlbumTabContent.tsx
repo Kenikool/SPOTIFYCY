6c5ac5ba425f39f9913b6a8e42e1f6f45f427b29
Kenikool <114859959+Kenikool@users.noreply.github.com>
2025-07-08 10:26:41 +0100

    Admin_06 album_tab_content and album Table

--- frontend/src/pages/admin/components/AlbumTabContent.tsx
+++ frontend/src/pages/admin/components/AlbumTabContent.tsx
@@ -1,7 +1,34 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Music } from "lucide-react";
 import React from "react";
-
+import AddAlbumDialog from "./AddAlbumDialog";
+import AlbumTable from "./Albumtable";
 const AlbumTabContent = () => {
-  return <div>AlbumTabContent</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex items-center justify-between">
+          <div>
+            <CardTitle className="flex items-center gap-2">
+              <Music className="size-5 text-violet-500" />
+              Albums Library
+            </CardTitle>
+            <CardDescription>Manage your albums</CardDescription>
+          </div>
+          <AddAlbumDialog />
+        </div>
+      </CardHeader>
+      <CardContent>
+        <AlbumTable />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default AlbumTabContent;
